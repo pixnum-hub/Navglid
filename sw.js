f5dcda0ca6cbd508d2cf69d0b264d495161a03c3
Manik Roy <pixnum@gmail.com>
2026-01-18 14:33:21 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = "navglid-v1";
+
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(cacheName).then(cache=>{
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>{
+      return res || fetch(e.request);
+    })
+  );
+});
